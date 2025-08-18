573803f70c1fdb7c001bc7b90ee41a17bb168fd6
lobov <lobov.vitaliy@gmail.com>
2025-08-18 16:11:12 +0300

    fix:

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/api/ping", handlers.PingHandler)
+	mux.HandleFunc("/api", handlers.PingHandler)
 
 	log.Println("🚀 Backend started on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
